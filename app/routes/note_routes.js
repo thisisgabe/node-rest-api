ca149d29e252cac098be0cc95a0f9a9fbc2a9c14
Gabriel Velasquez <gabriel.velasquez@jackinthebox.com>
2017-11-30 13:18:02 -0800

    changed mongodb to local instance, added data folder to gitignore

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -3,7 +3,16 @@
 module.exports = function(app, db) {
     app.post('/notes', (req, res)=> {
         //you'll create your note here
-        console.log(req.body)
-        res.send('Hello')
+        const note = { text: req.body.body, title: req.body.title };
+        db.collection('notes').insert(note, (err, result) => {
+            if (err)
+            {
+                res.send({ 'error': 'an error has occured'});
+            }
+            else
+            {
+                res.send(result.ops[0]);
+            }
+        });
     });
 };
