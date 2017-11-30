f5c5c0289048caa1f2b58ea7dee24e575a84a608
Gabriel Velasquez <gabriel.velasquez@jackinthebox.com>
2017-11-30 12:06:33 -0800

    set up the body parser to get the body as a json object

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -3,6 +3,7 @@
 module.exports = function(app, db) {
     app.post('/notes', (req, res)=> {
         //you'll create your note here
+        console.log(req.body)
         res.send('Hello')
     });
 };
